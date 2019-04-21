5fada51d8f4dbd8f06e1102f0200ab02b8aa08ab
tnsatbhs <tnsatbhs@yahoo.com>
2019-04-21 09:07:32 -0400

    added necessary files, updated GradebookController

--- src/main/java/GradebookController.java
+++ src/main/java/GradebookController.java
@@ -12,7 +12,33 @@ public class GradebookController {
 	}
 	
 	@RequestMapping(path = "/secondary/{id}", method = RequestMethod.POST)
-	public void createStudent(@PathVariable String name, @PathVariable String grade)
+	public void createSecondary(@PathVariable Long id)
+	{
+		//do something		
+	}
+	
+	@RequestMapping(path = "/gradebook/{id}/student/{name}/grade/{grade}", method = RequestMethod.POST)
+	public void createStudent(@PathVariable Long id, @PathVariable String name,
+			@PathVariable String grade)
+	{
+		//do something		
+	}
+	
+	@RequestMapping(path = "/gradebook/{name}", method = RequestMethod.PUT)
+	public void updateGradebook(@PathVariable String name)
+	{
+		//do something	
+	}
+	
+	@RequestMapping(path = "/secondary/{id}", method = RequestMethod.PUT)
+	public void updateSecondary(@PathVariable Long id)
+	{
+		//do something		
+	}
+	
+	@RequestMapping(path = "/gradebook/{id}/student/{name}/grade/{grade}", method = RequestMethod.PUT)
+	public void updateStudent(@PathVariable Long id, @PathVariable String name,
+			@PathVariable String grade)
 	{
 		//do something		
 	}
