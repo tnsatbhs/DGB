5c4024cae040d14ccc0c5f609287b42f48dd78b7
alexander <alexander.ressler@gmail.com>
2019-04-21 19:56:38 -0700

    Helper to determine the running server

--- src/main/java/dgb/GradebookService.java
+++ src/main/java/dgb/GradebookService.java
@@ -1,12 +1,23 @@
 package dgb;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.client.RestTemplate;
 
+
 public class GradebookService {
-	
+
+
 	public String getString (String entityUrl) {
 		RestTemplate restTemplate = new RestTemplate();
 		return restTemplate.getForObject(entityUrl, String.class);
 	}
-	
+
+	public void sayPort () {
+		System.out.println("PORT: " + Application.ctx.getEnvironment().getProperty("local.server.port"));
+	}
+
+
+	public Gradebook create (Gradebook gradebook) {
+		return  gradebook;
+	}
+
 }
