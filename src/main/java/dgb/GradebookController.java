ba1dab4ce90e750aed75c121d7fd7d29cb9b6213
alexander <alexander.ressler@gmail.com>
2019-04-23 18:26:35 -0700

    Start using GradebookService

--- src/main/java/dgb/GradebookController.java
+++ src/main/java/dgb/GradebookController.java
@@ -9,83 +9,90 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import dgb.GradebookService.GradebookException;
+
 @RestController
 public class GradebookController {
-	
+
 	Integer id;
 	ArrayList<Student> students;
 	Student student;
 	//the above variables will not be used in the final code, they are just so return types can be satisfied in stubs
-	
+
+
 	@Autowired
-	GradebookRepository gradebookRepo;
-	
+	GradebookService gradebookService;
+
+
+
 	@RequestMapping(path = "/gradebook/{name}", method = RequestMethod.POST)
-	public Integer createGradebook(@PathVariable String name)
+	public Gradebook createGradebook(@PathVariable String name)
 	{
 		return createGradebookOp(name);
 	}
-	
+
 	@RequestMapping(path = "/secondary/{id}", method = RequestMethod.POST)
 	public void createSecondary(@PathVariable Integer id)
 	{
 		//create secondary copy of gradebook, cannot be done on primary server
 		//most of this will be done via logic and communication between apps
 	}
-	
+
 	@RequestMapping(path = "/gradebook/{id}/student/{name}/grade/{grade}", method = RequestMethod.POST)
 	public void createStudent(@PathVariable Integer id, @PathVariable String name,
 			@PathVariable String grade)
 	{
 		//Von - add error checking here, throw exception if gradebook doesnt exist
-		Optional<Gradebook> opt = gradebookRepo.findById(id);
-		Gradebook tempG = opt.get();
-		Student tempS = new Student();
-		tempS.setName(name);
-		tempS.setGrade(grade);
-		tempG.addStudent(tempS);
-		gradebookRepo.save(tempG);
-		//add student and grade, can not be done on secondary, changes must flow to secondary		
+		try {
+			gradebookService.createStudent(id, name, grade);
+		} catch (GradebookException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
 	@RequestMapping(path = "/gradebook/{name}", method = RequestMethod.PUT)
-	public Integer updateGradebook(@PathVariable String name)
+	public Gradebook updateGradebook(@PathVariable String name)
 	{
 		return createGradebookOp(name);
 	}
-	
+
 	@RequestMapping(path = "/secondary/{id}", method = RequestMethod.PUT)
 	public void updateSecondary(@PathVariable Integer id)
 	{
 		//create secondary copy of gradebook, cannot be done on primary server
 		//most of this will be done via logic and communication between apps
 	}
-	
+
 	@RequestMapping(path = "/gradebook/{id}/student/{name}/grade/{grade}", method = RequestMethod.PUT)
 	public void updateStudent(@PathVariable Integer id, @PathVariable String name,
 			@PathVariable String grade)
 	{
-		//add student and grade, can not be done on secondary, changes must flow to secondary		
+		//add student and grade, can not be done on secondary, changes must flow to secondary
 	}
-	
+
 	@RequestMapping(path = "/gradebook", method = RequestMethod.GET,
 			produces={"text/xml;charset=utf-8"})
 	public List<Gradebook> getAllGradebooks()
 	{
 		//get all gradebooks on this server, including primary and secondary copies
-		return gradebookRepo.findAll();
+		return gradebookService.getAllGradebooks();
 	}
-	
+
 	@RequestMapping(path = "/gradebook/{id}/student", method = RequestMethod.GET,
 			produces={"text/xml;charset=utf-8"})
 	public List<Student> getAllStudents(@PathVariable Integer id)
 	{
 		//Von - add error checking here, throw exception if gradebook doesnt exist
-		Optional<Gradebook> tempG = gradebookRepo.findById(id);
-		return tempG.get().getAllStudents();
-		//get all students from gradebook, can be done on primary or secondary copy
+		try {
+			return gradebookService.getAllStudents(id);
+		} catch (GradebookException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
-	
+
 	@RequestMapping(path = "/gradebook/{id}/student/{name}", method = RequestMethod.GET,
 			produces={"text/xml;charset=utf-8"})
 	public Student getStudent(@PathVariable Integer id, @PathVariable String name)
@@ -93,31 +100,35 @@ public class GradebookController {
 		//get student information, can be done on primary or secondary copy
 		return student;
 	}
-	
+
 	@RequestMapping(path = "/gradebook/{id}", method = RequestMethod.DELETE)
 	public void deleteGradebook(@PathVariable Integer id)
 	{
-		//delete gradebook, must be done on primary server, deletion also deletes secondary copies		
+		//delete gradebook, must be done on primary server, deletion also deletes secondary copies
 	}
-	
+
 	@RequestMapping(path = "/secondary/{id}", method = RequestMethod.DELETE)
 	public void deleteSecondary(@PathVariable Integer id)
 	{
-		//delete secondary, must be done on secondary server, does not affect primary copy		
+		//delete secondary, must be done on secondary server, does not affect primary copy
 	}
-	
+
 	@RequestMapping(path = "/gradebook/{id}/student/{name}", method = RequestMethod.DELETE)
 	public void deleteStudent(@PathVariable Integer id, @PathVariable String name)
 	{
-		//delete gradebook, must be done on primary server, deletion auto updates secondary copies	
+		//delete gradebook, must be done on primary server, deletion auto updates secondary copies
 	}
-	
-	public Integer createGradebookOp(String name)
+
+	public Gradebook createGradebookOp(String name)
 	{
-		Gradebook gradebook = new Gradebook();
-		gradebook.setName(name);
-		gradebookRepo.save(gradebook);
-		return gradebook.getId();	
+		try {
+			return gradebookService.create(name);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("IT FAILED");
+		}
+		return null;
 	}
 
 }
--- src/main/java/dgb/GradebookService.java
+++ src/main/java/dgb/GradebookService.java
@@ -1,9 +1,22 @@
 package dgb;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 
+@Service
 public class GradebookService {
+
+	public class GradebookException extends Exception {
+		private static final long serialVersionUID = 1L;
+	}
+
+	@Autowired
+	public GradebookRepository gradebookRepo;
 
 
 	public String getString (String entityUrl) {
@@ -16,8 +29,41 @@ public class GradebookService {
 	}
 
 
-	public Gradebook create (Gradebook gradebook) {
-		return  gradebook;
+	public Gradebook create (String gradebookName) throws Exception {
+		Gradebook gradebook = new Gradebook();
+		gradebook.setName(gradebookName);
+		if (gradebook == null) throw new Exception("NO GRADEBOOK REPO");
+		gradebookRepo.save(gradebook);
+		return gradebook;
+	}
+
+
+	public List<Gradebook> getAllGradebooks () {
+		return gradebookRepo.findAll();
+	}
+
+
+	public List<Student> getAllStudents (Integer gradebookId) throws GradebookException {
+		Optional<Gradebook> opt = gradebookRepo.findById(gradebookId);
+		if (!opt.isPresent()) {
+			throw new GradebookService.GradebookException();
+		}
+		Gradebook gradebook = opt.get();
+		return gradebook.getAllStudents();
+	}
+
+	public void createStudent (Integer gradebookId, String studentName, String studentGrade) throws GradebookException {
+		Optional<Gradebook> opt = gradebookRepo.findById(gradebookId);
+		if (!opt.isPresent()) {
+			throw new GradebookService.GradebookException();
+		}
+		Gradebook gradebook = opt.get();
+		Student student = new Student();
+		student.setName(studentName);
+		student.setGrade(studentGrade);
+		gradebook.addStudent(student);
+		gradebookRepo.save(gradebook);
+		// TODO: add student and grade, can not be done on secondary, changes must flow to secondary
 	}
 
 }
