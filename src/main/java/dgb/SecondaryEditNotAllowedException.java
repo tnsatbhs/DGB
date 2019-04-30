a015c2892e302fda6b3d5f137206c4dd2b93d986
Rashmi <rashmilko2@gmail.com>
2019-04-29 22:19:20 -0400

    New exception file to handle secondary server can edit primary resource
    New exception file to handle secondary server can edit primary resource

---
+++ src/main/java/dgb/SecondaryEditNotAllowedException.java
@@ -0,0 +1,21 @@
+package dgb;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Secondary server can not edit resource")
+public class SecondaryEditNotAllowedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public SecondaryEditNotAllowedException(String exception)
+    {
+        super(exception);
+    }
+
+}
+
+
+
+
+
